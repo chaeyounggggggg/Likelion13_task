c01cbbea0fd5abeb5fa12aa5836144e681dcacc8
chaeyoung <olivia0827@naver.com>
2025-04-04 19:42:15 +0900

    Likelion13_task

---
+++ timer/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import TimerComponent from './TimerComponent';
+import { useState } from 'react';
+
+function App() {
+  return (
+    <TimerComponent></TimerComponent>
+  );
+}
+
+export default App;
